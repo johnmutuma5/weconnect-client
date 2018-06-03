b37f26ae3ee447260f5305a472e48ae522ca8dc7
John Mutuma <johnmutuma5@gmail.com>
2018-06-03 05:19:18 +0300

    [Chore] Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        This will be the main content of different pages
+      </Layout>
+    );
+  }
+}
+
+export default App;
