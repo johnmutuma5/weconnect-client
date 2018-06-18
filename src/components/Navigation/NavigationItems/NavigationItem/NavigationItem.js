afe0a28de4b292d5e2b3340d334aff260c80e193
John Mutuma <johnmutuma5@gmail.com>
2018-06-18 07:16:04 +0300

    [Chore] Allow access to hash and url query strings on Link

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 const navigationItem = (props) => (
     <li>
-        <Link to={ props.link_to }>{ props.children }</Link>
+        <Link to={ {
+            pathname: props.link_to,
+            hash: props.hash,
+            search: props.queryString
+        } }>{ props.children }</Link>
     </li>
 );
 
