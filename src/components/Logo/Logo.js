8399b1d09a42aff6b07e68ce24188753449b5c1f
John Mutuma <johnmutuma5@gmail.com>
2018-06-18 16:39:27 +0300

    [Chore] Adjust navigation styles

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,13 @@ import './Logo.css';
 
 const logo = (props) => (
     <div className="logo">
-        <img src={ props.src } alt={ props.alt_text } />
+        <div>
+            <img src={ props.src } alt={ props.alt_text } />
+        </div>
+        <div className="synopsis">
+            <p>{ 'Put your businesses on a global platform...' }</p>
+            <p>{ 'Looking to buy?? Find other businesses in your location' }</p>
+        </div>
     </div>
 );
 
