bf1c0c77bab987d18bc5c2e47bd42b612abd8b22
John Mutuma <johnmutuma5@gmail.com>
2018-06-19 07:35:22 +0300

    [Chore] Add side drawer

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Aux from '../../hoc/Aux';
 import Toolbar from './Toolbar/Toolbar';
+import SideDrawer from './SideDrawer/SideDrawer';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-    <div className='top-bar'>
-        <Toolbar />
-    </div>
+    <Aux>
+        <SideDrawer />
+        <div className='top-bar'>
+            <Toolbar />
+        </div>
+    </Aux>
 );
 
 export default mainNavigation;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SideDrawer.css';
+
+const sideDrawer = (props) => (
+    <div className='SideDrawer'>
+        sideDrawer
+    </div>
+);
+
+export default sideDrawer;
