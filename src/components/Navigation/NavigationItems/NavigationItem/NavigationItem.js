d4f1e2eaee34eba0e206bf26c29bb11b2a461429
John Mutuma <johnmutuma5@gmail.com>
2018-06-21 20:57:03 +0300

    [Chore] Reduce clutter in JSX

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,15 +3,26 @@ import { PropTypes } from 'prop-types';
 import './NavigationItem.css';
 import { NavLink } from 'react-router-dom';
 
-const navigationItem = (props, context) => (
-    <li onClick={ context.handleToggleSideDrawer } className='NavigationItem'>
-        <NavLink to={ {
-            pathname: props.link_to,
-            hash: props.hash,
-            search: props.queryString
-        } }>{ props.children }</NavLink>
-    </li>
-);
+const navigationItem = (props, context) => {
+    const to = {
+        pathname: props.link_to,
+        hash: props.hash,
+        search: props.queryString
+    };
+
+    const attribs = {
+        onClick: context.handleToggleSideDrawer,
+        className: 'NavigationItem'
+    }
+
+    return (
+        <li { ...attribs } >
+            <NavLink to={ to }>
+                { props.children }
+            </NavLink>
+        </li>
+    );
+};
 
 navigationItem.contextTypes = {
     handleToggleSideDrawer: PropTypes.func.isRequired
