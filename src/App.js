e7cd5ff73fdcb82efe7be08d40e991fb58bc5e08
John Mutuma <johnmutuma5@gmail.com>
2018-06-22 05:34:04 +0300

    [Chore] Add Businesses Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import { Route } from 'react-router-dom';
 
+
+
 class App extends Component {
   render() {
     return (
-      <Layout>
-            <Route exact path="/" render={ (props)=> <div>{ 'hello home' }</div> } />
-            <Route path="/businesses" render={ (props)=> <div>{ 'Hello Businesses' }</div> } />
-      </Layout>
+        <Layout>
+            <Route exact path="/businesses" component={ Businesses } />
+        </Layout>
     );
   }
 }
