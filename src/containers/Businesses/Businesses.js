92ac4ab9eb14873094675decbca2a1b30f7022e6
John Mutuma <johnmutuma5@gmail.com>
2018-06-22 05:56:12 +0300

    [Chore] Add place holder data

--- src/containers/Businesses/Businesses.js
+++ src/containers/Businesses/Businesses.js
@@ -3,12 +3,38 @@ import React from 'react';
 class Businesses extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            businesses: [
+                {
+                    id: '1000',
+                    name: 'Andela Kenya',
+                    location: 'TRM, Drive',
+                    category: 'Software'
+                },
+                {
+                    id: '1001',
+                    name: 'Sleek',
+                    location: 'Ridgeways, Kiambu Rd',
+                    category: 'Fashion'
+                }
+            ]
+        }
     }
 
     render() {
+        const businesses = this.state.businesses.map((business) => (
+                <div key={ business.id }>
+                    name: { business.name }
+                    <br />
+                    location: { business.location }
+                    <br />
+                    catergory: { business.category }
+                </div>
+        ));
+
         return (
             <div>
-                { 'Hello Businesses' }
+                { businesses }
             </div>
         );
     }
