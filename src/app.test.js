650dd8d39a635c471b05224dfdc99ee44b061efe
John Mutuma <johnmutuma5@gmail.com>
2018-07-06 13:32:38 +0300

    [Chore] Add tests for App and ResourcesProvider

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,27 @@
-/// sanity test
+import App from './App';
+import ResourcesProvider, { createStore } from './store';
+import weConnectReducer from './store/baseReducer';
 
-describe('testing setUp is Ok.', () => {
-    test('it is ok', () => {
+describe('<App />', () => {
+    test('global testing variables ok', () => {
+        /// sanity test
         expect.assertions(3);
         expect(shallow).not.toBeNull();
         expect(React).not.toBeNull();
         expect(localStorage).not.toBeNull();
+    });
+
+    test('ResourcesProvider renders <App />', () => {
+        const store = createStore(weConnectReducer);
+
+        const wrapper = shallow(<ResourcesProvider store={store}>
+                                    <App />
+                                </ResourcesProvider>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('App renders correctly', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
     })
 })
