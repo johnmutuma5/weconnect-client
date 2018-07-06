a23c534a6efe629c7e069fd5eb6dd010622f3890
John Mutuma <johnmutuma5@gmail.com>
2018-07-06 18:40:02 +0300

    [Chore] Adjust Styles

--- src/containers/BusinessProfile/PrimaryBusinessInfo/PrimaryBusinessInfo.js
+++ src/containers/BusinessProfile/PrimaryBusinessInfo/PrimaryBusinessInfo.js
@@ -1,29 +1,33 @@
 import React from 'react';
 
+import './PrimaryBusinessInfo.css';
+
 const primaryBusinessInfo = (props) => {
     console.log(props)
     return (
         <div className="business_details">
-            <div className="quick_details draw-right">
-                <h3>{ props.name }</h3>
-                <hr />
-                <div className="owner">
-                    <p><span>Business Owner:</span> { props.owner.name }</p>
+            <article>
+                <div className="quick_details draw-right">
+                    <h3>{ props.name }</h3>
+                    <hr />
+                    <div className="owner">
+                        <p><span>Business Owner:</span> { props.owner.name }</p>
+                    </div>
+                    <div className="business_category">
+                        <p><span>Business Category:</span> { props.category }</p>
+                        <p><span>Specialization:</span> { props.specialization }</p>
+                    </div>
+                    <div className="location">
+                        <p><span>{ 'Location' }:</span> { props.location } </p>
+                    </div>
+                    <div className="mobile">
+                        <p><span>{ 'Mobile' }:</span> { props.mobile } </p>
+                    </div>
+                    <div className="registration_date">
+                        <p><span>Registration date:</span> { props.registrationDate } </p>
+                    </div>
                 </div>
-                <div className="business_category">
-                    <p><span>Business Category:</span> { props.category }</p>
-                    <p><span>Specialization:</span> { props.specialization }</p>
-                </div>
-                <div className="location">
-                    <p><span>{ 'Location' }:</span> { props.location } </p>
-                </div>
-                <div className="mobile">
-                    <p><span>{ 'Mobile' }:</span> { props.mobile } </p>
-                </div>
-                <div className="registration_date">
-                    <p><span>Registration date:</span> { props.registrationDate } </p>
-                </div>
-            </div>
+            </article>
             <div className="background_overlay" />
         </div>
     );
--- src/containers/BusinessProfile/SocialMedia/SocialMedia.js
+++ src/containers/BusinessProfile/SocialMedia/SocialMedia.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
+import './SocialMedia.css';
+
 const socialMedia = (props) => {
     return (
-        <div class="social_media flex_row">
-          <div class="fb">
-            <a href="#" title='Facebook'><i class="fa fa-facebook-official" aria-hidden="true" style={{fontSize:'24px', color:'#3b5998'}}></i></a>
-          </div>
-          <div class="tw">
-            <a href="#" title='Twitter'><i class="fa fa-twitter" aria-hidden="true" style={{
-                fontSize:'24px', color:'#55acee'
-            }}></i></a>
-          </div>
-          <div class="ig">
-            <a href="#" title="Instagram"> <i class="fa fa-instagram" style={{
-                fontSize:'24px', color:'#bc2a8d'
-            }}></i></a>
-          </div>
-        </div>
+        <article id='socialMedia'>
+            <div class="social_media flex_row">
+              <div class="fb">
+                <a href="#" title='Facebook'><i class="fa fa-facebook-official" aria-hidden="true" style={{fontSize:'24px', color:'#3b5998'}}></i></a>
+              </div>
+              <div class="tw">
+                <a href="#" title='Twitter'><i class="fa fa-twitter" aria-hidden="true" style={{
+                    fontSize:'24px', color:'#55acee'
+                }}></i></a>
+              </div>
+              <div class="ig">
+                <a href="#" title="Instagram"> <i class="fa fa-instagram" style={{
+                    fontSize:'24px', color:'#bc2a8d'
+                }}></i></a>
+              </div>
+            </div>
+        </article>
     );
 }
 
